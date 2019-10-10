b796bdfaff64c22fad48201b3e62f6030b20bcee
bendjibenz <bendjibenz@users.noreply.github.com>
2019-10-10 10:54:04 +0100

    Create aya_hadi_et_nhbs.py

---
+++ aya_hadi_et_nhbs.py
@@ -0,0 +1,15 @@
+Z = clf.predict(data_samples)
+#Z = Z.reshape(xx.shape)
+plt.figure(1)
+#plt.pcolormesh(xx, yy, Z) # Affiche les déductions en couleurs pour les couples x,y
+
+# Plot also the training points
+#plt.scatter(data[:, 0], data[:, 1], c=target)
+colors = ['violet', 'yellow', 'red']
+C = [colors[x] for x in Z]
+
+plt.scatter(xx.ravel(), yy.ravel(), c=C)
+plt.xlim(xx.min() - .1, xx.max() + .1)
+plt.ylim(yy.min() - .1, yy.max() + .1)
+plt.xlabel('Longueur du sepal (cm)')
+plt.ylabel('Largueur du sepal (cm)');
